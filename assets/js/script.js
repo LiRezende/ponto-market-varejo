24e3409972c2f2bc0076552e6be8ede061c72296
LiRezende <librusamolin@gmail.com>
2021-09-13 13:59:57 -0300

    Formatação de carousel de testemunhos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
-// Menu Mobile
-window.onload = function() {
-    document.querySelector(".menuMobile").addEventListener("click", function(){
-        if(document.querySelector(".menu nav ul").style.display == 'flex') {
-            document.querySelector(".menu nav ul").style.display = 'none';
-        } else {
-            document.querySelector(".menu nav ul").style.display = 'flex';
-        }
+// Testimonial Slides
+let slides = document.querySelectorAll(".slide-container");
+let index = 0;
 
-    })
+function next() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
 }
+
+function previous() {
+  slides[index].classList.remove("active");
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add("active");
+}
+
+// Menu Mobile
+window.onload = function () {
+  document.querySelector(".menuMobile").addEventListener("click", function () {
+    if (document.querySelector(".menu nav ul").style.display == "flex") {
+      document.querySelector(".menu nav ul").style.display = "none";
+    } else {
+      document.querySelector(".menu nav ul").style.display = "flex";
+    }
+  });
+};

24257d634e84a00a07352c34820dcf6c68678c66
LiRezende <librusamolin@gmail.com>
2021-10-20 10:29:10 -0300

    Ajuste nos slides de testemunhos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 let slides = document.querySelectorAll(".slide-container");
 let index = 0;
 
-setInterval(function next() {
+setInterval(function slide() {
   slides[index].classList.remove("active");
   index = (index + 1) % slides.length;
   slides[index].classList.add("active");
